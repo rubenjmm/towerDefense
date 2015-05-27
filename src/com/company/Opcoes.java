f780265773b34678d8ded813ac3bbe9be1f58065
azuzel <ricardo.rmp.90@gmail.com>
2015-05-27 07:53:45 +0100

    added difficulty

--- src/com/company/Opcoes.java
+++ src/com/company/Opcoes.java
@@ -25,6 +25,9 @@ public class Opcoes {
 
     private int tempo_entre_spawn_mobs;
 
+
+    private int difficulty_type=0;
+
     public Opcoes(){
 
     }
@@ -58,7 +61,86 @@ public class Opcoes {
     }
 
 
+    public void  easy_difficulty() {
 
+        difficulty_type=1;
+        if ( Main.getLevel()==1) {
+
+            number_mobs_t1=1;
+            number_mobs_t2=2;
+
+            life_mobs_t1=100;
+            life_mobs_t2=150;
+
+            atack_mobs_t1=3;
+            atack_mobs_t2=2;
+
+            coin_mobs_t1=5;
+            coin_mobs_t2=10;
+
+            raio_b1=220;
+            atack_speed_b1=1500;
+            atack_b1=15;
+
+            tempo_entre_spawn_mobs=2500;
+
+
+        }
+    }
+
+    public void normal_difficulty() {
+
+        difficulty_type=2;
+        if ( Main.getLevel()==1) {
+
+            number_mobs_t1=3;
+            number_mobs_t2=3;
+
+            life_mobs_t1=120;
+            life_mobs_t2=170;
+
+            atack_mobs_t1=4;
+            atack_mobs_t2=3;
+
+            coin_mobs_t1=8;
+            coin_mobs_t2=15;
+
+            raio_b1=220;
+            atack_speed_b1=1500;
+            atack_b1=15;
+
+            tempo_entre_spawn_mobs=3000;
+
+
+        }
+    }
+
+    public void hard_difficulty() {
+
+        difficulty_type=3;
+        if ( Main.getLevel()==1) {
+
+            number_mobs_t1=7;
+            number_mobs_t2=6;
+
+            life_mobs_t1=120;
+            life_mobs_t2=170;
+
+            atack_mobs_t1=3;
+            atack_mobs_t2=2;
+
+            coin_mobs_t1=5;
+            coin_mobs_t2=10;
+
+            raio_b1=220;
+            atack_speed_b1=1500;
+            atack_b1=15;
+
+            tempo_entre_spawn_mobs=4500;
+
+
+        }
+    }
 
 
 //////////////////////////// GETTER'S ////////////////////////////
